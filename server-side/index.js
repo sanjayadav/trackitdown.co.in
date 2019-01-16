16552cedf4f25807f212d69222b82fe003824b6b
sanjayadav <sy5233@gmail.com>
2019-01-16 15:16:29 +0530

    Changed CORS URL from * to trackitdown.co.in

--- server-side/index.js
+++ server-side/index.js
@@ -34,7 +34,7 @@ app.use(express.static(path.join(__dirname, 'client')));
 
 
 app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "trackitdown.co.in");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     next();
